5db75cfb6d557b930f7a1468dd9f035f2023a990
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-10-19 18:57:10 -0700

    Tasks updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,2 @@
-# Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
-
-# Increase Limit to hard for user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/4/50000/' /etc/security/limits.conf",
-  path => '/usr/local/bin/:/bin/'
-}
-
-# Increase Limits to soft for user
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path => '/usr/local/bin/:/bin/'
-}
+# Changes the OS configuration so that it is possible to login with the holberton user and open a file without any error message
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
