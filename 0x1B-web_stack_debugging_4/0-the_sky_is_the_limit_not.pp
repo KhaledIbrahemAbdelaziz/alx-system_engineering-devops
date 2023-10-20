e53cac5e009263abbfe36244f121f2ac3c2cb96a
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-10-19 18:53:47 -0700

    Tasks updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,5 @@
 # Fixing the number of failed requests to get to 0
-
-# Increase the value in the default file of nginx of ULIMIT
 exec { 'fix--for-nginx':
-  # Modify the value
-  command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
-  # Determine the path to it
-  path => '/usr/local/bin/:/bin/',
-}
-
-# Restart
-exec { 'nginx-restart':
-  # Restarting
-  command => '/etc/init.d/nginx restart',
-  # Determine the path
-  path => '/etc/init.d/',
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
